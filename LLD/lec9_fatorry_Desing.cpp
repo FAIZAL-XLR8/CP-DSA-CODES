790755892dc108d7098394497b99198ef8719076
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-15 14:01:51 +0530

    done with simple factory

--- LLD/lec9_fatorry_Desing.cpp
+++ LLD/lec9_fatorry_Desing.cpp
@@ -25,12 +25,24 @@ class FactoryBurger{ // a factory class who's sole work is to return the correct
     }
     Burger* generateBurger(string & type)
     {
+        try{
                 if (type == "standard") return new StandardBurger();
                 else if (type == "normal") return new NormalBurger();
+                else throw invalid_argument("not correct type");
+        }
+        catch (const invalid_argument& err)
+        {
+            cout << err.what() <<" pls do right \n";
+            return '\0';
+        }
     }
 
 };
 int main()
 {
-
+    string request_of_client = "elel";
+    FactoryBurger* fb = new FactoryBurger();
+    Burger* bg = fb ->generateBurger(request_of_client);
+    if(bg)bg->prepare();
+    else cout <<"give correct type \n";
 }
