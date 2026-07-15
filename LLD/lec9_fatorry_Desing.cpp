9e60997d834b95a939e056fe47ac34d1f6f71aaa
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-15 13:43:13 +0530

    learning lld favtory design

---
+++ LLD/lec9_fatorry_Desing.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Burger{ //abstract class because burger can be of several types
+    public:
+    virtual void  prepare() = 0;
+};
+
+int main()
+{
+
+}
