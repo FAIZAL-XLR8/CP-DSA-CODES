97ff78c18a3c96ba6c2da8488d50404ff66bc68b
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-15 17:02:15 +0530

    abstract fatory_abstract

--- LLD/factory_methdAndAbstract.cpp
+++ LLD/factory_methdAndAbstract.cpp
@@ -3,22 +3,52 @@ using namespace std;
 class Burger {
     public:
    virtual  void prepare_burger() = 0;
-
+    virtual ~Burger() {
+        cout <<"Burger_desturctor\n";
+    };
     
 };
 class Bread{
     public :
     virtual void prepare_bread() = 0;
+    virtual ~Bread() {
+        cout <<"Bread_destructor\n";
+    }
 };
 class NormalBread :public Bread{
     public :
-    void prepare_burger() override {
+    void prepare_bread() override {
         cout <<" normal burger preparing" << endl;
     }
-}
+};
+class StandardBread : public Bread {
+    public :
+    void prepare_bread()
+    {
+        cout <<"preparing stndard bread\n";
+    }
+};
+class NormalBurger : public Burger {
+    public :
+    void prepare_burger ()
+    {
+        cout <<"preparing normal burger\n";
+    }
+};
+class StandardBurger : public Burger {
+    public :
+    void prepare_burger ()
+    {
+        cout << " preparing standard burger\n";
+    }
+};
 class Factory{
+    public :
     virtual Burger* create_burger(string bgtype) = 0;
     virtual Bread* create_bread(string bdtype) = 0;
+    virtual ~Factory(){
+        cout <<"Factory_destructor\n";
+    };
 };
 class BurgerSingh : public Factory{
     public:
@@ -61,8 +91,8 @@ class BurgerKing : public Factory{
         
         try{
         cout << "creating burger from Burger Singh\n";
-        if (bgtype == "normal") return new WheatNormalBurger();
-        else if (bgtype == "standard") return new WheatStandardBurger();
+        if (bgtype == "normal") return new NormalBurger();
+        else if (bgtype == "standard") return new StandardBurger();
         else throw invalid_argument("error no correct type selected");
         }
         catch (invalid_argument& err)
@@ -76,13 +106,13 @@ class BurgerKing : public Factory{
     {
 
         try{
-        if (data == "normal") return new WheatNormalBread();
-        else if (data == "standard") return new WheatStandardBread();
+        if (data == "normal") return new NormalBread();
+        else if (data == "standard") return new StandardBread();
         else throw invalid_argument("error not correct type selected");
         }
         catch(invalid_argument& err)
         {
-            cout <<err.what()<<" : error\n";
+            cout << err.what() << " : error\n";
         }
         return nullptr;
 
@@ -93,6 +123,14 @@ class BurgerKing : public Factory{
 
 int main()
 {
-
-
+    string data_bread = "normal";
+    string data_burger = "standard";
+    Factory* fct = new BurgerKing();
+    Bread* bd = fct->create_bread(data_bread);
+    Burger* bg = fct->create_burger(data_burger);
+   if(bg) bg->prepare_burger();
+   if(bd) bd->prepare_bread();
+   delete fct;
+   delete bg;
+   delete bd;
 }
