03e51cd1c64d0cec83a6447e213db765f9679e4d
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-15 13:51:58 +0530

    new logic

--- LLD/lec9_fatorry_Desing.cpp
+++ LLD/lec9_fatorry_Desing.cpp
@@ -5,6 +5,31 @@ class Burger{ //abstract class because burger can be of several types
     virtual void  prepare() = 0;
 };
 
+class NormalBurger : public Burger{
+    public :
+    void prepare() override {
+        cout << " preparing noraml burger \n";
+    }
+};
+class StandardBurger : public Burger {
+    public :
+    void prepare() override {
+        cout << " prepare the standard burger \n";
+    }
+};
+class FactoryBurger{ // a factory class who's sole work is to return the correct object of the particular class
+    public:
+
+    FactoryBurger()
+    {
+    }
+    Burger* generateBurger(string & type)
+    {
+                if (type == "standard") return new StandardBurger();
+                else if (type == "normal") return new NormalBurger();
+    }
+
+};
 int main()
 {
 
