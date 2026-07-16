d82eaea9ca6439f1b53a63edfce16dc0587a2267
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-16 23:20:08 +0530

    added some singleton

---
+++ LLD/SingletonDesign.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Singleton{
+    private :
+    Singleton()
+    {
+        cout <<"this is constructor\n";
+    }
+    static Singleton* instance;
+    public :
+    static Singleton* getInstance()
+    {
+        if (instance == nullptr)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+};
+Singleton* Singleton :: instance = nullptr; 
+
+int main()
+{
+    Singleton* obj1 = Singleton :: getInstance();
+}
