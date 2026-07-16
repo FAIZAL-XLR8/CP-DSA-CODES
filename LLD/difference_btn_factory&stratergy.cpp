78d485aca43dce5e2f8438d03092634241d24136
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-16 10:14:50 +0530

    learned so far

---
+++ LLD/difference_btn_factory&stratergy.cpp
@@ -0,0 +1,18 @@
+// Factory → used when you need different objects (different concrete types), decided once, at creation time. Client ends up holding one of several possible types.
+// Strategy → you already have your main object (e.g. Order, Car). It needs to perform some behavior, and that behavior has multiple possible implementations. You abstract that behavior into its own interface, write multiple concrete implementations of it, and the main object holds a pointer/reference to that abstraction as a member — swappable, even after the main object already exists.
+robot //abstract class
+{
+    Pointers hona chahiye of behaviours to chnage at runtime
+    //Dlyable*;
+    //Swimmable*;
+}
+class Humaonid robot{
+Humanoid(properites) : robot()
+{
+    
+}
+}
+int main ()
+{
+    Robot/ClassName of main_existing object = new Robot_type(its properties sent)
+}
