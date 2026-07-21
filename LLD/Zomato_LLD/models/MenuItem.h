dcd989b94d27aa6917dd6ea674649f40a1d66802
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-21 22:29:59 +0530

    added menu item

---
+++ LLD/Zomato_LLD/models/MenuItem.h
@@ -0,0 +1,36 @@
+#ifndef MENU_ITEM
+#define MENU_ITEM
+#include <bits/stdc++.h>
+using namespace std;
+class MenutItem{
+    private :
+    string name;
+    static int curr_item_id;
+    double price;
+    public :
+    MenuItem (const string &name, const double& price)
+    {
+        this -> name = name;
+        this -> price = price;
+        this -> item_id = ++curr_item_id;
+    }
+    int getCode() return this-> curr_item_id;
+    string getname() return this-> name;
+    double getPrice() return this-> price;
+    void setPrice (const double &price)
+    {
+        this -> price = price;
+    }
+    void setName (const string &name)
+    {
+        this -> name = name;
+    }
+    ~MenuItem()
+    {
+        cout << "Menu item destructor called\n";
+    }
+};  
+int MenuItem :: curr_item_id = 0;
+
+
+#endif
