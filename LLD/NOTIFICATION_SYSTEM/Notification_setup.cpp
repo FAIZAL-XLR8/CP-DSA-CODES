a255dae66a7e4c248c01e9cb51520710d3a8e690
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-26 16:46:34 +0530

    added notification_system

---
+++ LLD/NOTIFICATION_SYSTEM/Notification_setup.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// we need first  to create the message
+//since creating the message would need dynamically to add
+//many behaviours we use decorator pattern
+class INotification{
+    public :
+    
+};
