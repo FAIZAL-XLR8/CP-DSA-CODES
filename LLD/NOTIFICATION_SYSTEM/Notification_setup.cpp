e0d2c2125347fb34c81f22f765cd7cce2afb3c27
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-26 16:50:25 +0530

    added decorator

--- LLD/NOTIFICATION_SYSTEM/Notification_setup.cpp
+++ LLD/NOTIFICATION_SYSTEM/Notification_setup.cpp
@@ -5,5 +5,21 @@ using namespace std;
 //many behaviours we use decorator pattern
 class INotification{
     public :
-    
+    virtual string sendNotification() = 0;
 };
+class BaseNotification : public INotification{
+    public :
+    string sendNotification() override {
+        return "here is the base hello message\n";
+    }
+};
+class IDecoratorNotification  : public INotification{
+    protected :
+    INotification* nt;
+    public :
+    IDecoratorNotification(INotification* nt) : nt(nt){}
+    virtual string sendNotification() = 0;
+};
+class TimeStampDecorator : public IDecoratorNotification{
+    
+}
