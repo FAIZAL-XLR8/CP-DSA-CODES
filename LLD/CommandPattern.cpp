bb6c20ac3b72d0fb008b2592fa8bdc34bc0e5b39
FAIZAL-XLR8 <slif858@gmail.com>
2026-07-27 22:29:21 +0530

    in oprocess

--- LLD/CommandPattern.cpp
+++ LLD/CommandPattern.cpp
@@ -3,14 +3,43 @@ using namespace std;
 class Remote{
     private :
     static Remote* instance;
+    vector<ICommand*> list; //this list serves as the list of 
+    // commands that the remote has to offer
+    vector<bool> isPressed;
     Remote()
-    {}
+    {
+        list.resize(4,nullptr);
+        isPressed.resize(4, false);
+    }
 
     public :
-    ICommand* cmd;
     static Remote* getInstance()
     {
         return instance;
+    }
+    void setCommand(Icommand* cmd, int idx)
+    {
+        try{
+        if (idx < list.size() and idx > 0 and list[idx] != nullptr)
+        {
+            list[idx] = cmd;
+        }
+        else{
+            throw  logical_error("Wrong input of command!")
+            }
+         }
+         catch(const logical_error &e)
+         {
+            cout << e.what() << endl;
+         }
+        
+    }
+    void pressButton(int idx)
+    {
+            if (isPressed[idx] == true)
+            {
+                list[idx] -> execute();
+            }
     }
     
 };
@@ -19,6 +48,46 @@ class ICommand {
     public :
     virtual void execute() = 0;
     virtual void undo() = 0;
+};
+class Fan{
+    public :
+    void on() cout << "Fan is switched on\n";
+    void off() cout << "Fan is switched off\n";
+};
+class AC {
+    void on() cout << "AC is on\n";
+    void off() cout << "AC is off\n";
+};
+class FanCommand : public ICommand {
+    private :
+    Fan* fan;
+    public :
+    FanCommand(Fan* fan)
+    {
+        this -> fan = fan;
+    }
+    void execute() override {
+        fan -> on();
+    }
+    void undo()
+    {
+        fan->off();
+    }
+};
+class ACCommand : public ICommand {
+    public :
+    AC* ac;
+    ACCommand(AC* ac){
+        this -> ac = ac;
+    }
+    void execute()
+    {
+        ac -> on();
+    }
+    void off()
+    {
+        ac -> off();
+    }
 };
 int main()
 {
